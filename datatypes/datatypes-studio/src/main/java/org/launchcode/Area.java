f52be73abf93c989a459640709bd9e7d0157f3bb
Mohammed0704 <116527392+Mohammed0704@users.noreply.github.com>
2023-08-21 21:27:16 -0400

    new classes Area and Circle

---
+++ datatypes/datatypes-studio/src/main/java/org/launchcode/Area.java
@@ -0,0 +1,10 @@
+package org.launchcode;
+import java.util.Scanner;
+public class Area {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter a radius:");
+        double userRadius = input.nextDouble();
+        System.out.println("The area of a circle of radius " + userRadius + " is: " + Circle.getArea(userRadius));
+    }
+}
